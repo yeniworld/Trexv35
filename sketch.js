f994a679a1ea1ee436727dac5839432c2598df5c
yeniworld <93004106+yeniworld@users.noreply.github.com>
2022-01-11 23:34:06 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -79,7 +79,7 @@ function draw() {
     score = score + Math.round(getFrameRate()/60);
     ground.velocityX = -(6 + 3*score/100);
   
-    if((touches.length>0 || keyDown("SPACE")) && trex.y >= height-110) {
+    if((touches.length>0 || keyDown("SPACE")) && trex.y >= height-120) {
       trex.velocityY = -12;
       touches = [];
     }
